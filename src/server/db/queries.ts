5ed6a19b2fd122e3f6a60ff45629ef6aafb77d71
morg-ma <mahmoud.ahmed.25.9.24@gmail.com>
2025-12-31 11:21:57 +0200

    refactor: add ordering to folder and file queries in the database

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -29,11 +29,16 @@ function getFolders(folderId: bigint) {
   return db
     .select()
     .from(foldersTable)
-    .where(eq(foldersTable.parent, folderId));
+    .where(eq(foldersTable.parent, folderId))
+    .orderBy(foldersTable.id);
 }
 
 function getFiles(folderId: bigint) {
-  return db.select().from(filesTable).where(eq(filesTable.parent, folderId));
+  return db
+    .select()
+    .from(filesTable)
+    .where(eq(filesTable.parent, folderId))
+    .orderBy(filesTable.id);
 }
 
 function getFolderById(folderId: bigint) {
